f1cbea82917599036d32f28d19a1024be06de071
SagunNakarmi <sagun@qitplus.com>
2024-10-10 23:03:41 +1000

    Add cheese interface and update cheese-list

--- src/app/components/cheese-list/cheese-list.component.ts
+++ src/app/components/cheese-list/cheese-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CheeseItemComponent } from "../cheese-item/cheese-item.component";
 
 @Component({
   selector: 'app-cheese-list',
   standalone: true,
-  imports: [],
+  imports: [CheeseItemComponent],
   templateUrl: './cheese-list.component.html',
   styleUrl: './cheese-list.component.scss'
 })
---
+++ src/app/interfaces/cheese.interface.ts
@@ -0,0 +1,8 @@
+export interface Cheese {
+    id: number;
+    name: string;
+    imageUrl: string;
+    pricePerKilo: number;
+    color: string;
+  }
+  
