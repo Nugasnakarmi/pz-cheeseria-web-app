274521ffb880b430dc65521abc56486a742204d5
SagunNakarmi <sagun@qitplus.com>
2024-10-10 23:03:06 +1000

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CheeseItemComponent } from '../app/components/cheese-item/cheese-item.component';
+import { CheeseListComponent } from './components/cheese-list/cheese-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: 'cheese-item',      component: CheeseItemComponent },
+    {
+      path: 'cheese-list',
+      component: CheeseListComponent,
+      data: { title: 'Chese List' }
+    },
+    { path: '',
+      redirectTo: '/cheese-list',
+      pathMatch: 'full'
+    },];
