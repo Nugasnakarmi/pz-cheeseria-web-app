52c3cd88b570b5752cbd8d8dde5874f01e5084b7
SagunNakarmi <sagun@qitplus.com>
2024-10-10 23:44:01 +1000

    Update cheese list component with dummy data

--- src/app/components/cheese-list/cheese-list.component.ts
+++ src/app/components/cheese-list/cheese-list.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
-import { CheeseItemComponent } from "../cheese-item/cheese-item.component";
+import { CheeseItemComponent } from '../cheese-item/cheese-item.component';
+import { Cheese } from '../../interfaces/cheese.interface';
 
 @Component({
   selector: 'app-cheese-list',
   standalone: true,
   imports: [CheeseItemComponent],
   templateUrl: './cheese-list.component.html',
-  styleUrl: './cheese-list.component.scss'
+  styleUrl: './cheese-list.component.scss',
 })
 export class CheeseListComponent {
-
+  cheeseList: Cheese[] = [
+    {
+      id: 0,
+      name: 'Camembert',
+      imageUrl:
+        'https://www.cheese.com/media/img/cheese-suggestion/manchego_1280x800_1.jpg',
+      pricePerKilo: 3.5,
+      color: 'beige',
+    },
+    {
+      id: 1,
+      name: 'Cheddar',
+      imageUrl:
+        'https://www.cheese.com/media/img/cheese-suggestion/manchego_1280x800_1.jpg',
+      pricePerKilo: 3.5,
+      color: 'red',
+    },
+    {
+      id: 2,
+      name: 'Mozzarella',
+      imageUrl:
+        'https://www.cheese.com/media/img/cheese-suggestion/manchego_1280x800_1.jpg',
+      pricePerKilo: 3.5,
+      color: 'white',
+    },
+    {
+      id: 3,
+      name: 'Gouda',
+      imageUrl:
+        'https://www.cheese.com/media/img/cheese-suggestion/manchego_1280x800_1.jpg',
+      pricePerKilo: 3.5,
+      color: 'beige',
+    },
+    {
+      id: 4,
+      name: 'Gruyere',
+      imageUrl:
+        'https://www.cheese.com/media/img/cheese-suggestion/manchego_1280x800_1.jpg',
+      pricePerKilo: 3.5,
+      color: 'white',
+    },
+  ];
 }
