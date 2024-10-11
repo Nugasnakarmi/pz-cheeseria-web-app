e1eacfed4de2f38e561ac92c01702bfc3cb40bfd
SagunNakarmi <sagun@qitplus.com>
2024-10-12 08:56:40 +1000

    Add CRUD methods to service

--- src/app/services/cheese.service.ts
+++ src/app/services/cheese.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Cheese } from '../interfaces/cheese.interface';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheeseService {
+  private apiUrl = `${environment.apiUrl}/cheese`;
+  http = inject(HttpClient);
 
-  constructor() { }
+  getAllCheese$(): Observable<Cheese[]> {
+    const getAllCheeseUrl = `${this.apiUrl}/all`;
+    return this.http.get<Cheese[]>(getAllCheeseUrl);
+  }
+
+  getCheeseById(id: number): Observable<Cheese> {
+    return this.http.get<Cheese>(`${this.apiUrl}/${id}`);
+  }
+
+  addCheese$(cheese: Cheese): Observable<Cheese> {
+    const addCheeseUrl = `${this.apiUrl}`;
+    return this.http.post<Cheese>(this.apiUrl, cheese);
+  }
+
+  updateCheese(id: number, cheese: Cheese): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, cheese);
+  }
+
+  deleteCheese(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
