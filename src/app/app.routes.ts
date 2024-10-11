f44940e2718571af05fde9cd3441db6e8339f8dd
SagunNakarmi <sagun@qitplus.com>
2024-10-12 08:56:18 +1000

    Update app routes and clean app comp template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,25 @@
 import { Routes } from '@angular/router';
 import { CheeseItemComponent } from '../app/components/cheese-item/cheese-item.component';
 import { CheeseListComponent } from './components/cheese-list/cheese-list.component';
+import { CheeseFormComponent } from './components/cheese-form/cheese-form.component';
+import { CheeseShopComponent } from './components/cheese-shop/cheese-shop.component';
 
-export const routes: Routes = [ 
-    { path: 'cheese-item',      component: CheeseItemComponent },
-    {
-      path: 'cheese-list',
-      component: CheeseListComponent,
-      data: { title: 'Chese List' }
-    },
-    { path: '',
-      redirectTo: '/cheese-list',
-      pathMatch: 'full'
-    },];
+export const routes: Routes = [
+  { path: '', redirectTo: '/cheese-shop', pathMatch: 'full' },
+  { path: 'cheese-item', component: CheeseItemComponent },
+  {
+    path: 'cheese-list',
+    component: CheeseListComponent,
+    data: { title: 'Cheese List' },
+  },
+  {
+    path: 'cheese-form',
+    component: CheeseFormComponent,
+    data: { title: 'Cheese Form' },
+  },
+  {
+    path: 'cheese-shop',
+    component: CheeseShopComponent,
+    data: { title: 'Cheese Shop' },
+  },
+];
